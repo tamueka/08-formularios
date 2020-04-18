1edc356a9c71aa0d1cbf5208571ea7ed06e52282
tamueka <tamueka@hotmail.com>
2020-04-18 13:20:01 +0200

    Formulario reactivo: validaciones sincronas + estados del formulario tiempo real

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -22,10 +22,14 @@ export class ReactiveComponent implements OnInit {
   crearFormulario(){
 
     this.formulario = this.fb.group({
-      nombre: ['Samuel'],
-      apellido: ['Ruiz'],
-      correo: ['tamueka@hotmail.com']
-    })
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      correo: [
+        '',
+        [ Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$') ],
+      ],
+    });
 
   }
 
