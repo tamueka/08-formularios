4dceaeba9965264b6f1efb79d793208bb5b24c12
tamueka <tamueka@hotmail.com>
2020-04-18 17:06:32 +0200

    Formulario reactivo: validacion visual de los campos anidados

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -4,59 +4,86 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
-
   formulario: FormGroup;
 
   constructor(private fb: FormBuilder) {
-
     this.crearFormulario();
-
-   }
-
-  ngOnInit(): void {
   }
 
-  get NombreNoValido(){
-    return this.formulario.get('nombre').invalid && this.formulario.get('nombre').touched;
+  ngOnInit(): void {}
+
+  get nombreNoValido() {
+    return (
+      this.formulario.get('nombre').invalid &&
+      this.formulario.get('nombre').touched
+    );
   }
 
-  get ApellidoNoValido(){
-    return this.formulario.get('apellido').invalid && this.formulario.get('apellido').touched;
+  get apellidoNoValido() {
+    return (
+      this.formulario.get('apellido').invalid &&
+      this.formulario.get('apellido').touched
+    );
   }
 
-  get CorreoNoValido(){
-    return this.formulario.get('correo').invalid && this.formulario.get('correo').touched;
+  get correoNoValido() {
+    return (
+      this.formulario.get('correo').invalid &&
+      this.formulario.get('correo').touched
+    );
   }
 
-  crearFormulario(){
+  get distritoNoValido() {
+    return (
+      this.formulario.get('direccion.distrito').invalid &&
+      this.formulario.get('direccion.distrito').touched
+    );
+  }
 
+  get ciudadNoValido() {
+    return (
+      this.formulario.get('direccion.ciudad').invalid &&
+      this.formulario.get('direccion.ciudad').touched
+    );
+  }
+
+  crearFormulario() {
     this.formulario = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(5)]],
       correo: [
         '',
-        [ Validators.required,
-        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$') ],
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
       ],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
-        ciudad: ['', Validators.required]
-      })
+        ciudad: ['', Validators.required],
+      }),
     });
-
   }
 
-  guardar(){
-    console.log( this.formulario )
+  guardar() {
+    console.log(this.formulario);
 
-    if( this.formulario.invalid ){
-      return Object.values( this.formulario.controls ).forEach( control => {
-        control.markAsTouched();
+    if (this.formulario.invalid) {
+
+      return Object.values(this.formulario.controls).forEach(control => {
+
+        if ( control instanceof FormGroup ) {
+
+          return Object.values(control.controls).forEach(control => control.markAsTouched() );
+
+        } else {
+          control.markAsTouched();
+        }
+
       });
     }
   }
-
 }
